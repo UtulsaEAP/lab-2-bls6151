b73368560c27b6ff4294f3705da5ac497d59a311
Braden Stanfield <bls6151@utulsa.edu>
2024-02-02 15:02:12 -0600

    all done

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -5,6 +5,7 @@ def caffeine():
     after6 = caffeine_mg /2
     after12 = caffeine_mg /4
     after24 = caffeine_mg /8
+    
     print("After 6 hours: " + f'{after6:.2f}' + " mg")
     print("After 12 hours: " +f'{after12:.2f}'+ " mg")
     print("After 24 hours: " +f'{after24:.2f}'+ " mg")
