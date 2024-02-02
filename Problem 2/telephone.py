e00acf387e3d55280d2ee177f9856de0141b81cb
Braden Stanfield <bls6151@utulsa.edu>
2024-02-02 14:35:53 -0600

    Problem 2

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,5 +1,10 @@
 def telephone():
     phone_number = int(input())
     ''' Type your code here. '''
+    print("(" + str((phone_number // 1000000000)) + str((phone_number % 1000000000 // 100000000)) + 
+          str(phone_number % 100000000 // 10000000) + ") " + str(phone_number % 10000000 // 1000000) +
+          str(phone_number % 1000000 // 100000) + str(phone_number % 100000 // 10000) + "-" + 
+          str(phone_number % 10000 // 1000) + str(phone_number % 1000 // 100) + str(phone_number % 100 // 10) +
+          str(phone_number % 10 // 1))
 if __name__ == "__main__":
     telephone()
