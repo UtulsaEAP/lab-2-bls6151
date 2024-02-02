60c219bc7089c2922a76dfabcdc7d4ea2cc36203
Braden Stanfield <bls6151@utulsa.edu>
2024-02-02 15:13:17 -0600

    really think im done this time

--- Problem 3/caffeine.py
+++ Problem 3/caffeine.py
@@ -4,7 +4,7 @@ def caffeine():
     ''' Type your code here. '''
     after6 = float(caffeine_mg /2)
     after12 = float(caffeine_mg /4)
-    after24 = float(caffeine_mg /8)
+    after24 = float(caffeine_mg /16)
     
     print("After 6 hours: " + f'{after6:.2f}' + " mg")
     print("After 12 hours: " + f'{after12:.2f}'+ " mg")
